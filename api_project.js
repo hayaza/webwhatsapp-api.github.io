0797592337e2665dc72f6e742723a31a51ea037b
Azmi Hayaza <hayaza@gmail.com>
2021-07-02 23:13:16 +0700

    first commit

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "whatsapp-web-api",
+  "version": "0.1.0",
+  "description": "API to use whatsapp web",
+  "title": "Whatsapp Web API",
+  "url": "https://web-whatsapp-api/api/v1",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-06-30T16:37:50.477Z",
+    "url": "https://apidocjs.com",
+    "version": "0.28.1"
+  }
+});
